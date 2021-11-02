afbf1afa63b14cbfffa627191a07ed64ef66d69d
Ponefar <adrienwolf@laposte.net>
2021-11-02 20:29:06 +0100

    url github

--- src/components/doc/index.js
+++ src/components/doc/index.js
@@ -5,7 +5,7 @@ const Doc = () => {
         <div className="moyenFormulaire">
             <h1 className="title center">Documentation</h1>
             <p className="texte">Projet réalise entièrement en React JS</p><br />
-            <p className="texte">Lien Github : </p><br />
+            <p className="texte">Lien Github : https://github.com/Ponefar/fruition.git</p><br />
             
         </div>
     )
