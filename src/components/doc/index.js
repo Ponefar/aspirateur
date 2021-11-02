2293451c97cc906cf473a67505d519fd19c8cd41
Ponefar <adrienwolf@laposte.net>
2021-11-02 20:34:31 +0100

    css github link

--- src/components/doc/index.js
+++ src/components/doc/index.js
@@ -5,7 +5,7 @@ const Doc = () => {
         <div className="moyenFormulaire">
             <h1 className="title center">Documentation</h1>
             <p className="texte">Projet réalise entièrement en React JS</p><br />
-            <p className="texte">Lien Github : https://github.com/Ponefar/fruition.git</p><br />
+            <a target="_blank" href="https://github.com/Ponefar/fruition.git" className="texte royalBleu hover">Lien Github : https://github.com/Ponefar/fruition.git</a><br />
             
         </div>
     )
